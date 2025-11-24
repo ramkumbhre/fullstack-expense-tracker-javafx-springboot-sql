47e005abd6d510f17039a69b8a2adef09e866893
ram kumbhre <ramkumbhre321@gmail.com>
2025-11-24 19:31:56 +0530

    V.1

---
+++ expense-tracker-client/src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import org.example.utils.ViewNavigator;
+import org.example.views.DashboardView;
+import org.example.views.LoginView;
+import org.example.views.SignUpView;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Expense Tracker");
+        ViewNavigator.setMainStage(stage);
+        new LoginView().show();
+    }
+}
